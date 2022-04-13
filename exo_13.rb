3c8e4edd968e00fb906b4441793ac1f0432783e1
foucauld <foucauldbeaumont@yahoo.fr>
2022-04-13 08:58:35 +0200

    changement exo

--- exo_13.rb
+++ exo_13.rb
@@ -1,7 +1,11 @@
 str = []
 
 50.times do |i|
-    str << "jean.dupont.#{i + 1}@email.fr"
+    if i < 9
+        str << "jean.dupont.0#{i + 1}@email.fr"
+      else
+        str << "jean.dupont.#{i + 1}@email.fr"
+      end
 end
 
 puts str
