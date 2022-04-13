bcae2d4e070ddafa1c36d07578e360e7514905a1
foucauld <foucauldbeaumont@yahoo.fr>
2022-04-13 08:45:22 +0200

    new commit

--- exo_10.rb
+++ exo_10.rb
@@ -2,14 +2,14 @@ require 'date'
 
 print "Année de naissance : " 
 user_birth = gets.chomp.to_i
-user_age = 0
+user_age = -1
 
-while user_birth < Date.today.year
+while user_birth <= Date.today.year
     user_birth = user_birth + 1
     if user_age < 9
-        puts " En #{user_birth} tu avais 0#{user_age += 1} ans"
+        puts " En #{user_birth - 1} tu avais 0#{user_age += 1} ans"
     else 
-        puts " En #{user_birth} tu avais #{user_age += 1} ans"
+        puts " En #{user_birth - 1} tu avais #{user_age += 1} ans"
     end
  end
 
